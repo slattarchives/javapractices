a2d3a17b7101575c0c13434fa92f75639946c732
slattarchives <144155523+slattarchives@users.noreply.github.com>
2024-01-05 20:45:47 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Package4;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+}
